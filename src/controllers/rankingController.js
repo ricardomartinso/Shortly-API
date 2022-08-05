607c1e6cb096c111624358eb6c2f76857394ec75
Ricardo <ricardomartinso.dev@gmail.com>
2022-08-05 11:45:08 -0300

    ranking with users without links

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -2,8 +2,10 @@ import { connection } from "../databases/postgres.js";
 
 export async function getRank(req, res) {
   const { rows: rank } = await connection.query(
-    `SELECT u.id, u.name, COUNT("linksUser"."userId") as "linksCount", SUM("linksUser"."visitCount") as "visitCount" FROM users AS u JOIN "linksUser" ON u.id = "linksUser"."userId" GROUP BY "linksUser"."userId", u.id ORDER BY "visitCount" DESC LIMIT 10`
+    `SELECT u.id, u.name, COUNT("linksUser"."userId") as "linksCount", COALESCE(SUM("linksUser"."visitCount"),0) as "visitCount" FROM users AS u LEFT JOIN "linksUser" ON u.id = "linksUser"."userId" GROUP BY "linksUser"."userId", u.id ORDER BY "visitCount" DESC LIMIT 10`
   );
+
+  const rankMap = rank.map;
 
   res.status(200).send(rank);
 }
