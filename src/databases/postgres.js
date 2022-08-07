ba277eb4f73758c272ebc44824872ebf2a62df09
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-08-07 11:32:02 -0300

    prepare for deploy

--- src/databases/postgres.js
+++ src/databases/postgres.js
@@ -1,10 +1,16 @@
 import pg from "pg";
 import dotenv from "dotenv";
 
+const { Pool } = pg;
 dotenv.config();
 
-const { Pool } = pg;
-
-export const connection = new Pool({
+const databaseConfig = {
   connectionString: process.env.DATABASE_URL,
-});
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+
+const connection = new Pool(databaseConfig);
+
+export default connection;
