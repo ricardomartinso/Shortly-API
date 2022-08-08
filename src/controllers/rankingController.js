aa90025ea66d6278a9002ab87d9bc35f92765b67
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-08-08 17:33:07 -0300

    adjust string to integer on ranking

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -2,10 +2,8 @@ import { connection } from "../databases/postgres.js";
 
 export async function getRank(req, res) {
   const { rows: rank } = await connection.query(
-    `SELECT u.id, u.name, COUNT("linksUser"."userId") as "linksCount", COALESCE(SUM("linksUser"."visitCount"),0) as "visitCount" FROM users AS u LEFT JOIN "linksUser" ON u.id = "linksUser"."userId" GROUP BY "linksUser"."userId", u.id ORDER BY "visitCount" DESC LIMIT 10`
+    `SELECT u.id, u.name, COUNT("linksUser"."userId")::int as "linksCount", COALESCE(SUM("linksUser"."visitCount"),0)::int as "visitCount" FROM users AS u LEFT JOIN "linksUser" ON u.id = "linksUser"."userId" GROUP BY "linksUser"."userId", u.id ORDER BY "visitCount" DESC LIMIT 10`
   );
-
-  const rankMap = rank.map;
 
   res.status(200).send(rank);
 }
